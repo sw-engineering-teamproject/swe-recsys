a60b0633a38246221c523de7be7122d85d87bb09
DONGWOO KIM <66837024+polarpolar914@users.noreply.github.com>
2024-05-29 15:42:27 +0900

    feat: DB 유사도검색(#3)

--- fastapi_service.py
+++ fastapi_service.py
@@ -12,3 +12,27 @@ async def load_faiss_cpu_db(db_index):
     from langchain_community.vectorstores import FAISS
     db = FAISS.load_local(db_index, embeddings, allow_dangerous_deserialization=True)
     return db
+
+async def search_faiss_cpu_db(query, db, k=5):
+    from langchain_community.embeddings import HuggingFaceEmbeddings
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2", encode_kwargs={'normalize_embeddings': True}
+    )
+    embedding_vector = await embeddings.aembed_query(query)
+    docs_and_scores = await db.asimilarity_search_with_score(query, k=k)
+    searched = []
+    for doc, score in docs_and_scores:
+        searched.append(doc.metadata['assignee_id'])
+        searched.append(doc.metadata['fixer_id'])
+
+    result = []
+    cnt = 0
+    for i in searched:
+        if i not in result:
+            result.append(i)
+            cnt += 1
+        if cnt == 3:
+            break
+    return result
+
